7e019513984eaefcde564592671607b00a15f636
Neki <nedsonroni14@gmail.com>
2026-06-30 00:59:24 -0300

    feat: declare and sum first variables in java

---
+++ Curso JAVA/src/Modulo_02/CalculadoraPerformance.java
@@ -0,0 +1,11 @@
+package Modulo_02;
+
+public class CalculadoraPerformance {
+    public static void main(String[] args) {
+        //VARIAVEIS
+        int primeiroNumero = 10;
+        int segundoNumero = 20;
+        var resultado = primeiroNumero + segundoNumero;
+        System.out.println(resultado);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,1 @@
+public class Main { public static void main(String[] args) {System.out.println("Proved by Mentorship: High-perfomance Engine Online."); } }
