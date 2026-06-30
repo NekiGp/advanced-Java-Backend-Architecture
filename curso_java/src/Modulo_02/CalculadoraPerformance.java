34ba06eae600090dde36e2e2602abf3a2be05993
Neki <nedsonroni14@gmail.com>
2026-06-30 16:11:02 -0300

    feat: implement database server efficiency calculator

--- curso_java/src/Modulo_02/CalculadoraPerformance.java
+++ curso_java/src/Modulo_02/CalculadoraPerformance.java
@@ -2,10 +2,15 @@ package Modulo_02;
 
 public class CalculadoraPerformance {
     public static void main(String[] args) {
-        //VARIAVEIS
-        int primeiroNumero = 7;
-        int segundoNumero = 6;
-        var resultado = (primeiroNumero + segundoNumero) / 2.0;
-        System.out.println(resultado);
+        //Criando variaveis (long para os totais, double para o tempo)
+        long totalRequisicoes = 500000L;
+        long requisicoesComSucesso = 485000L;
+        double tempoLimiteMilisegundos = 150.0;
+
+        var taxaSucesso = (requisicoesComSucesso * 100.0) / totalRequisicoes;
+
+        System.out.println("--- RELATÓRIO DE PERFORMANCE ---");
+        System.out.println("Total de Requisições: " + totalRequisicoes);
+        System.out.println("Taxa de Sucesso do Servidor: " + taxaSucesso + "%");
     }
 }
